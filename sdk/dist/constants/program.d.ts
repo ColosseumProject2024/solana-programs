173a617a1302cd90989af769f1886695b901f80d
Marcelo G Feitoza <71825192+marcelofeitoza@users.noreply.github.com>
2024-04-08 17:47:07 -0300

    fix: removed dist folder

--- sdk/dist/constants/program.d.ts
+++
@@ -1,1 +0,0 @@
-export declare const DEVOLT_PROGRAM_ID = "A8Tx8hQwFY2mDuRG89dxiLBac1V3rJtkpPKvktc7uMkw";
--- sdk/dist/constants/program.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.DEVOLT_PROGRAM_ID = void 0;
-exports.DEVOLT_PROGRAM_ID = 'A8Tx8hQwFY2mDuRG89dxiLBac1V3rJtkpPKvktc7uMkw';
